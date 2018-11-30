c25a3bda9a84900ab2b77579d74df62e2976f4a1
Jeffrey T Gosselin <jgoz.email@gmail.com>
2018-11-30 20:01:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  i 
+  data.each do |key, val|
+    val.each do |k, v|
+      pigeons = val.values.flatten.uniq
+      
+      if pigeon_list.has_key?(v)
+        pigeon_list[v] = key
+        
+      else
+        pigeon_list.store(v, key)
+      end
+      
+     binding.pry 
+    end
+  
+  end
+  
 end
